d40598d6e6e9b8dd604af6692ddaaadeaa797e01
sagarjaid23 <sagarjaid23@gmail.com>
2022-03-11 10:30:16 +0530

    Assignment Day5

---
+++ Basic Program/Basic Program/Harmonic Number.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Program
+{
+    internal class Harmonic_Number
+    {
+        public Harmonic_Number()
+        {
+            IsHarmonic();
+        }
+        static void IsHarmonic()
+        {
+            Utility u = new Utility();
+            Console.Write("Enter the  n th number: ");
+            int num = u.ReadInt();
+            if (num > 0)
+            {
+                double sum = 0.0;
+                for (double i = 1; i <= num; i++)
+                {
+                    sum += 1 / i;
+                }
+                Console.WriteLine(num + " th Harmonic number is = " + sum);
+            }
+        }
+    }
+}
---
+++ Basic Program/Basic Program/Vowel or Not.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Program
+{
+    internal class Vowel_or_Not
+    {
+        public Vowel_or_Not()
+        {
+            IsVowelorNot();
+        }
+
+        static void IsVowelorNot()
+        {
+            Utility.u = new Utility();
+            string s;
+            Console.WriteLine("Enter Alphabet");
+            s = Console.ReadLine();
+
+
+            switch (s)
+            {
+                case "a": Console.WriteLine("Vowel"); break;
+                case "e": Console.WriteLine("Vowel"); break;
+                case "i": Console.WriteLine("Vowel"); break;
+                case "o": Console.WriteLine("Vowel"); break;
+                case "u": Console.WriteLine("Vowel"); break;
+                case "A": Console.WriteLine("Vowel"); break;
+                case "E": Console.WriteLine("Vowel"); break;
+                case "I": Console.WriteLine("Vowel"); break;
+                case "O": Console.WriteLine("Vowel"); break;
+                case "U": Console.WriteLine("Vowel"); break;
+                default:
+                    Console.WriteLine("Alphabet is Consonent"); break;
+
+            }
+        }
+
+    }
+    
+}
