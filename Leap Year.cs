0d0d672857fb0e5727297c78d563168b93150541
sagarjaid23 <99254738+sagarjaid23@users.noreply.github.com>
2022-03-11 10:39:16 +0530

    Add files via upload

---
+++ Leap Year.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Program
+{
+    internal class Leap_Year
+    {
+        public Leap_Year()
+        {
+            IsLeapYear();
+        }
+        static void IsLeapYear()
+        {
+            Utility u = new Utility();
+            Console.Write("Enter thee year: ");
+            int year = u.ReadInt();
+            if (year > 999 && year < 10000)
+            {
+                if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0)
+                {
+                    Console.WriteLine(year + " is a Leap Year....");
+                }
+                else
+                {
+                    Console.WriteLine(year + " is not a Leap Year....");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not valid Year............");
+            }
+        }
+    }
+}
---
+++ Odd Even Number.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Program
+{
+    internal class Odd_Even_Number
+    {
+        public Odd_Even_Number()
+        {
+            IsoddEvenNumber();
+        }
+
+        static void IsoddEvenNumber()
+        {
+            Utility.u = new Utility();
+            Console.WriteLine("Please Enter the number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Numer is Even Number");
+            }
+            else
+            {
+                Console.WriteLine("Number is Odd Number");
+            }
+        }
+    }
+}
